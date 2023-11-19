946d2d4c3bcc933409fe305aeec466e9fdc44d76
Sush <sushcloud12@gmail.com>
2023-11-19 00:58:50 +0000

    i

--- alb.tf
+++ alb.tf
@@ -2,13 +2,22 @@ resource "aws_lb" "alb" {
   name               = var.ALB_NAME
   internal           = var.INTERNAL    # if true private flase public ALB
   load_balancer_type = "application"
-  security_groups    = 
-  subnets            = 
+  security_groups    = var.INTERNAL ? [aws_security_group.alb_private.id] : [aws_security_group.alb_public.id]
+  subnets            = var.INTERNAL ? data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS : data.terraform_remote_state.vpc.outputs.PUBLIC_SUBNET_IDS
 
+## conditions in terraforms 
+## if we create a private alb then security group and subnets should come
+## up with private sg and subnet
 
+## if we select false in internal public load balncer will be created and 
+## sg and subnet should be public
+
+# The syntax of a conditional expression is as follows:
+# condition ? true_val : false_val
 
 
   tags = {
-    Environment = "roboshop-${var.ENV}-alb"
+    Name = var.ALB_NAME
   }
 }
+
--- sg.tf
+++ sg.tf
@@ -1,5 +1,6 @@
 # Creates public alb security group
 resource "aws_security_group" "alb_public" {
+  count = var.INTERNAL ? 0 : 1  
   name        = "roboshop-${var.ENV}-public-alb-sg"
   description = "Allows Only public traffic"
   vpc_id      = data.terraform_remote_state.vpc.outputs.VPC_ID
@@ -29,6 +30,7 @@ resource "aws_security_group" "alb_public" {
 
 # Creates private alb security group
 resource "aws_security_group" "alb_private" {
+  count = var.INTERNAL ? 1 : 0  
   name        = "roboshop-${var.ENV}-private-alb-sg"
   description = "Allows Only private traffic"
   vpc_id      = data.terraform_remote_state.vpc.outputs.VPC_ID
@@ -39,7 +41,7 @@ resource "aws_security_group" "alb_private" {
     from_port        = 80
     to_port          = 80
     protocol         = "tcp"
-    cidr_blocks      = ["data.terraform_remote_state.vpc.outputs.VPC_ID", "data.terraform_remote_state.vpc.outputs.DEFAULT_VPC_CIDR"]
+    cidr_blocks      = [data.terraform_remote_state.vpc.outputs.VPC_ID, data.terraform_remote_state.vpc.outputs.DEFAULT_VPC_CIDR]
 
   }
 
@@ -55,3 +57,8 @@ resource "aws_security_group" "alb_private" {
     Name = "roboshop-${var.ENV}-private-alb-sg"
   }
 }
+
+## here we have used count to make sure that the private sg is created when the internal is 
+## true and alb-private is called vice vesa for public
+## other wise when the terrforms tries to create the sgs both private and public
+## each time the modules are called . this will create a conflict
