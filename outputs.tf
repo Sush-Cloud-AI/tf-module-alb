36a1738df03b25411b39b1504c25bd2ca4732a47
Sush <sushcloud12@gmail.com>
2023-11-25 22:32:20 +0000

    i

--- outputs.tf
+++ outputs.tf
@@ -6,6 +6,6 @@ output "ALB_ADDRESS" {
     value   = aws_lb.alb.dns_name
 }
 
-output "LISTENER_ARN" {
-    value   = aws_lb_listener.private.*.arn
-}
+# output "LISTENER_ARN" {
+#     value   = aws_lb_listener.private.*.arn
+# }
